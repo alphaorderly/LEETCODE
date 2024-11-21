34299dde93a50f83982c233cb334820685204d92
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2024-11-21 21:19:37 +0900

    Time: 446 ms (45%), Space: 43 MB (59.65%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
@@ -1,30 +1,27 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        grid = [[0] * n for _ in range(m)]
-
-        ans = m * n - len(guards) - len(walls)
-
-        for r, c in guards:
-            grid[r][c] = -1
-
-        for r, c in walls:
-            grid[r][c] = -1
-
-        rd = [-1, 1, 0, 0]
-        cd = [0, 0, -1, 1]
-
-        for r, c in guards:
-            for i in range(4):
-                ml = 1
-                while True:
-                    rt = r + rd[i] * ml
-                    ct = c + cd[i] * ml
-                    if rt < 0 or rt >= m or ct < 0 or ct >= n or grid[rt][ct] == -1:
-                        break
-                    else:
-                        if grid[rt][ct] != -2:
-                            grid[rt][ct] = -2
-                            ans -= 1
-                    ml += 1
-
-        return ans
+        walls = set(map(tuple, walls))
+        guards = set(map(tuple, guards))
+        cell = [[0] * n for _ in range(m)]
+        for i, j in guards:
+            cell[i][j] = 2  # guard
+        for i, j in walls:
+            cell[i][j] = -1  # wall
+        move=[[-1,0],[1,0],[0,-1],[0,1]] # w s a d
+        for gi, gj in guards:
+            for dx, dy in move:
+                ni, nj = gi + dx, gj + dy
+                while 0 <= ni < m and 0 <= nj < n:
+                    if cell[ni][nj] == -1 or cell[ni][nj] == 2:
+                        break  # wall
+                    if cell[ni][nj] == 0:
+                        cell[ni][nj] = 1  # flag
+                    ni += dx
+                    nj += dy
+        unguarded = 0
+        for i in range(m):
+            for j in range(n):
+                if cell[i][j] == 0:
+                    unguarded += 1
+        
+        return unguarded
