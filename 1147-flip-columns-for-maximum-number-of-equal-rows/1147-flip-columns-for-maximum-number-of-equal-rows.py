a0a0aa70725d594dca05e2563ed2370a8e8b7aec
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2024-11-22 20:52:10 +0900

    Time: 135 ms (20.37%), Space: 20.2 MB (20.6%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
@@ -5,9 +5,7 @@ class Solution:
         for row in matrix:
             rev_row = [1 if n == 0 else 0 for n in row]
 
-            print(row, rev_row)
-
             patterns[str(row)] += 1
             patterns[str(rev_row)] += 1
-        
+
         return max(patterns.values())
