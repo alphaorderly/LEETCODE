a518c6bedbdd262a213ff3ff588e72c6c82e97ad
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2024-11-22 20:56:40 +0900

    Time: 66 ms (51.85%), Space: 20.1 MB (23.12%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
@@ -3,11 +3,8 @@ class Solution:
         patterns = defaultdict(int)
 
         for row in matrix:
-            rev_row = "".join('1' if n == 0 else '0' for n in row)
-            nor_row = "".join(str(n) for n in row)
-
-            patterns[nor_row] += 1
-            patterns[rev_row] += 1
-
+            # 기준 패턴: 첫 번째 값에 따라 0 또는 1로 시작
+            pattern = tuple(n ^ row[0] for n in row)  
+            patterns[pattern] += 1
 
         return max(patterns.values())
