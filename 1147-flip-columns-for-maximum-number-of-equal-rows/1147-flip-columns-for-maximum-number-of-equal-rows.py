d6685490a02a30fbfaa59a13f6bb45bb142e3952
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2024-11-22 20:55:00 +0900

    Time: 131 ms (20.37%), Space: 19.4 MB (39.2%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
@@ -3,9 +3,10 @@ class Solution:
         patterns = defaultdict(int)
 
         for row in matrix:
-            rev_row = tuple(1 if n == 0 else 0 for n in row)
+            rev_row = "".join('1' if n == 0 else '0' for n in row)
+            nor_row = "".join(str(n) for n in row)
 
-            patterns[tuple(row)] += 1
+            patterns[nor_row] += 1
             patterns[rev_row] += 1
 
 
