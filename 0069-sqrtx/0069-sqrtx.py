a27a4627522adac2510db84a0973b32ab22e4220
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2024-11-22 22:46:41 +0900

    Time: 3 ms (62.94%), Space: 16.6 MB (27.42%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,24 @@
+from math import floor
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        def f(c: float):
+            return c ** 2 - x
+
+        def f_d(c: float):
+            return 2 * c
+
+        def n_r(c: float):
+            return c - (f(c) / f_d(c))
+
+        prev = x / 2 + 1
+        init = x / 2 + 1
+
+        for i in range(100):
+            prev = init
+            init = n_r(init)
+            if abs(((init - prev) / init) * 100.0) < 0.0005:
+                break
+
+        return floor(init)
+        
